286009f5b2a14be30bdc8c5aae9d6c8b8717802e
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2024-04-18 12:03:01 -0400

    Create Insane Python Code.py

---
+++ Insane Python Code.py
@@ -0,0 +1,93 @@
+# Here are some INSANE things you can do with Python programming.
+# However, you wouldn't really want to do this as a programmer for real.
+# Let's just see how we can turn rational Python programming into an
+# insane Cobra. Let's have some insane fun with these Rubber Room
+# Python program examples. Note: this is how we Asperger's people do
+# programming when no one is looking; we deviate from the norm. lol
+
+# To fully understand all this Python insanity, you should be familiar
+# with Python programming in general to understand all this craziness.
+
+insane_Python_print_function = print
+
+insane_Python_print_function('''''''''Insane Python print() function.''''''''') # single quotes
+insane_Python_print_function("""""""Insane Python print() function.""""""""""""") # double quotes
+
+
+integer = int
+
+insane_Python_print_function(integer(3.0))
+
+
+integer = int(3.0)
+
+insane_Python_print_function(integer)
+
+
+insane_Python_input_function = input
+
+message = insane_Python_input_function('''''''''What is your name Please? ''''''''') # single quotes
+insane_Python_print_function('Hello',message.title()+'.','''''''''How are you?''''''''') # single quotes
+
+message = insane_Python_input_function("""""""What is your name Please? """"""""""""") # double quotes
+insane_Python_print_function('Hello',message.title()+'.',"""""""How are you?""""""""""""") # double quotes
+
+
+insane_Python_input_function = input('''''''''What is your name Please? ''''''''') # single quotes
+
+message = insane_Python_input_function
+insane_Python_print_function('Hello',message.title()+'.','''''''''How are you?''''''''') # single quotes
+
+insane_Python_input_function = input("""""""What is your name Please? """"""""""""") # double quotes
+
+
+message = insane_Python_input_function
+insane_Python_print_function('Hello',message.title()+'.',"""""""How are you?""""""""""""") # double quotes
+
+
+George_Bool = bool
+
+insane_Python_print_function(George_Bool(1>3))
+
+
+George_Bool = bool(1>3)
+
+insane_Python_print_function(George_Bool)
+
+
+insane_Python_print_function('I so love my','star'[::-1],'too much!')
+
+insane_Python_print_function('racecar'[::-1],'is racecar spelled backwards.')
+
+
+binary = bin
+hexadecimal = hex
+octal = oct
+
+insane_Python_print_function(binary(10))
+insane_Python_print_function(hexadecimal(10))
+insane_Python_print_function(octal(10))
+
+
+binary = bin(10)
+hexadecimal = hex(10)
+octal = oct(10)
+
+insane_Python_print_function(binary)
+insane_Python_print_function(hexadecimal)
+insane_Python_print_function(octal)
+
+# Normally, we have to use the programming standard 'self' word with def functions.
+# But who says we have to do it here? Afterall, we can do whatever we want here and
+# nobody can tell us any different. I'm simply showing you all what you can do in Python,
+# but programmers DO NOT LIKE THIS. But we do. Let's use the word 'insane_snake' in
+# place of 'self' with our def function example.
+
+def insane_function(insane_snake):
+    insane_Python_print_function('Python is an insane Cobra') # this is the screen text output
+
+insane_function("I'm a placeholder for 'self' that we changed to insane_snake")
+
+# Hope you guys are having as much fun as I am having. However, do not do this kind of
+# programming if you want a programming job; the Boss won't be your Boss for very long.
+# I showed you what not to do. lol
