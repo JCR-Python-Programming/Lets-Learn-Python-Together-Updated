53327d7286cfa94506337f278563ba4e0506a880
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2024-04-29 22:10:19 -0400

    Update Condensed Version Laser Wars Python program example.py

--- Condensed Version Laser Wars Python program example.py
+++ Condensed Version Laser Wars Python program example.py
@@ -11,26 +11,26 @@ laser_wars.title('LASER WARS')
 
 def random_colour_code():
     hex_chars = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
-    colour_code='#'
+    colour_code = '#'
     
     for i in range(0,6):
-        colour_code=colour_code+choice(hex_chars)
+        colour_code = colour_code+choice(hex_chars)
     return colour_code
 
-laser_canvas=Canvas(laser_wars,width=1920,height=1080,background='#000000')
-laser_canvas.grid(row=0,column=0)
+laser_canvas = Canvas(laser_wars,width = 1920,height = 1080,background = '#000000')
+laser_canvas.grid(row = 0,column = 0)
 
 while True:    
 
     for i in range(20):
-        random_width=randint(0,15)
+        random_width = randint(0,15)
         x = randint(-1920,1920)
         laser_canvas.create_line(
             randint(-1920+x,1920+x),
             randint(-1920+x,1920+x),
             randint(-1920+x,1920+x),
             randint(-1920+x,1920+x),
-            fill=random_colour_code(),
+            fill = random_colour_code(),
             width = random_width)
 
     laser_canvas.update()
