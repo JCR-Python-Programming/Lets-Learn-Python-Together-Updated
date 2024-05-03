94aa1bfd8674a08760cb1ca003da853c8c7b90a5
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2024-05-03 04:57:53 -0400

    Add files via upload

---
+++ Iron Maiden Number of the Beast Lyrics.py
@@ -0,0 +1,83 @@
+# Some Heavy Metal Python Fun for Iron Maiden Fans, including myself am!!
+
+# I still love this song, especially as a teenager. It was 1982 and I was smoking
+# some really good stuff when I first heard this song on someone's old school
+# ghetto blaster; we were all outside, just sitting around passing the fun all around.
+# Just something about the 1980's that were just simpler and better. It's so sad that
+# cellphones took that all away. No one physically speaks to anyone at all anymore.
+# I never ever see anyone throw a frisbee anymore at all. What happened to our
+# world, my generation grew up in? I sure do miss the Good Old 80's. Don't you!?
+
+# Created by Joseph C. Richardson, GitHub.com
+
+# Number of The Beast Python program example:
+
+# Written lyrics by Iron Maiden
+
+xof=(ord('f')+ord('o')+ord('x'))*2
+
+IRON_MAIDEN_Number_of_the_Beast_lyrics = f'''
+esruoc sti ekat live ym ekam ot rewop eht evah I
+ecrof eht evah I ,erif eht evah I nrub uoy ekam ll'I
+dna ydob ruoy ssessop ll'I dnA nruter lliw I ,kcab
+gnimoc m'I
+
+em dna uoy rof eno eht ,{xof}
+tsaeb eht fo rebmun eht ,{xof}
+
+seye rieht diova t'nac ,eziremsem ot meeS
+sedroh gnitnahc eht sdrawot nward leef I tuB
+?maerd yzarc emos tsuj ro ,laer eb llits siht naC
+wal eht mrofni tsum I ,no og t'nac sihT
+
+thginot no gniog si ecifircaS
+tsaeb eht fo rebmun eht ,{xof}
+
+enod si krow s'nataS ,nugeb sah lautir ehT
+thgirb gninrub era serif eht ,thgin eht nI yks eht
+ot dleh sdnah ,yrc ot trats yeht sA desiarp erew
+stnahc dercas dna dezalb sehcroT
+
+desaeler eb ot denwaps saw erif dna lleH
+tsaeb eht fo rebmun eht ,{xof}
+
+?lleH fo dnik emos tsuj ro laer rof siht lla saW
+tsiwt dna evom serugif krad ,tsim eht nI ?em
+gnihctaw enoemos saw ,ees ot dah tsuj I esuaC'
+kcab gnidloh esu on saw ,kcalb saw thgiN
+
+riapsed ot em sgnirb dna dnim ym stsiwt taht ecaf
+live ehT ereht syawla s'ti smaerd ym ni esuaC' ?em
+ta kcab gnirats dnim depraw ym fo snoitcelfer yeht
+ereW smaerd dlo ym ni was I tahw tsuJ
+
+?ysatnaf tsuj ton dna laer saw thgin taht was I tahw
+tahT ?eveileb I naC ?ees I did tahW dnim ym morf
+seiromem eht teg oT kniht ot emit dedeen I knalb
+saw dnim ym ,enola tfel I
+
+{xof} si rebmun stI rebmun namuh a si ti roF tsaeb
+eht fo rebmun eht nokcer gnidnatsrednu htah ohw
+mih teL trohs si emit eht swonk eh esuaceB htarw
+htiw tsaeb eht sdnes liveD eht roF aes dna htrae ho
+,uoy ot eoW
+'''
+print(IRON_MAIDEN_Number_of_the_Beast_lyrics[::-1])
+
+# This kind of music probably isn't everyone's cup of tea, but I sure loved it.
+# Iron Maiden was New Wave Digital Aged Futuristic Heavy Metal. They started
+# in 1979 and broke out in mid 1982 with Number of The Beast album. These
+# guys are still at it today. They have their very own futuristic New Wave electronic
+# Metal sound. Fastway was another New Wave sort of Punk Rock, but leaned into
+# Heavy Metal on a more experimental way. I used to call Fastway. Punk Zeppelin,
+# because the league singer sounded a bit like Robert Plant. These two bands were
+# indeed on my ghetto blaster, along with RUSH, my most favorite of any band. I love
+# Rush music. Futuristic Retro is what they are. They don't make music today like
+# they did in our day. Not even close. Rush, Moving Pictures and these two guys here
+# were my most favorites. I loved Pink Floyd Another Brick in the Wall. I blasted that
+# in my high school hallway really loud at lunch time. I got real heck for it. Boy I had
+# myself an hour detention for doing it. I cranked that sucker up so loud, it could
+# deafen anyone. I had a really expensive stereo ghetto blaster; a Panasonic with
+# detachable speakers on it. I cranked that btch right up and I would do it all again,
+# if I could. It costed me an hour after school, but the detention was so worth every
+# second of it. I hated school and it hated me just as much. lol
