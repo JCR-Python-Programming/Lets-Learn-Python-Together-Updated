b307d4a062043c45032b8fbea027a0222103c0b9
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2024-05-31 03:33:44 -0400

    Create Return Functions 2.py

---
+++ Return Functions 2.py
@@ -0,0 +1,49 @@
+# Try these Python program return function examples.
+
+def my_first_return_value(book): # (one variable parameter)
+    return "I am the actual value called 'Python'"
+
+print(my_first_return_value(
+    "placeholder argument value for the variable \
+'book' that contains the value 'Python' "))
+
+
+def my_second_return_value(book,python): # (two variable parameters)
+    return "I am the actual value called 'Python'"
+
+print(my_second_return_value(
+    "placeholder argument value for the variable 'book'",
+    "placeholder argument value for the variable 'Python'"))
+
+
+def my_third_return_value(book,python,program): # (three variable parameters)
+    return "I am the actual value called 'Python'"
+
+print(my_third_return_value(
+    "placeholder argument value for the variable 'book'",
+    "placeholder argument value for the variable 'Python'",
+    "placeholder argument value for the variable 'program'"))
+
+# Let's learn some def functions that return number values
+
+def my_first_return_num(num): # (one variable parameter)
+    return 1 # I am the actual value called 1
+
+print(my_first_return_num("placeholder argument value for the variable 'num1'"))
+
+
+def my_second_return_num(num1,num2): # (two variable parameters)
+    return 2 # I am the actual values called 2
+
+print(my_second_return_num(
+    "placeholder argument value for the variable 'num1'",
+    "placeholder argument value for the variable 'num2'"))
+
+
+def my_third_return_num(num1,num2,num3): # (three variable parameters)
+    return 3 # I am the actual values called 3
+
+print(my_third_return_num(
+    "placeholder argument value for the variable 'num1'",
+    "placeholder argument value for the variable 'num2'",
+    "placeholder argument value for the variable 'num3'"))
