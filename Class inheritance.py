0a8fc03178d0f388ac1245816c9d82e2258d1e0a
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2024-06-10 03:50:04 -0400

    Update Class inheritance.py

--- Class inheritance.py
+++ Class inheritance.py
@@ -1,4 +1,4 @@
-# Let's create a class inheritance using only the class name 'Parent'.
+# Let's create a class inheritance using only the class name 'Person'.
 # This will allow us to inherit properties from the Parent class to the
 # inherited class called 'Inherit'. Any attributes in the Parent or main
 # class are all passed onto the inherited class called 'Inherit'
