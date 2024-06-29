fa05a4581e0d1cdb46ec5ab28bd298c6d7df793a
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2024-06-28 20:08:57 -0400

    Update Extensive Python Program Examples.py

--- Extensive Python Program Examples.py
+++ Extensive Python Program Examples.py
@@ -402,6 +402,42 @@ print(multi_dim_list[2][1])
 
 print(multi_dim_list[2][2])
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Things you can do with tuples and lists.
+
+# Tuple Example:
+
+variable_1='dog','cat','bird','guppy'
+
+variable_2=(
+    'Grey Wolf','Huge Tigger',
+    'Macaw Parrot','Great White Shark')
+
+variable_3='John','Rob','Ron','Bob'
+
+variable_4='Mom','Dad','Brother','Sister'
+
+variable_5='friend','girlfriend','boyfriend','neighbour'
+
+for var1,var2,var3,var4,var5 in zip(variable_1,variable_2,variable_3,variable_4,variable_5):
+    print(f'My {var4} and my {var5} {var3} says my {var1} is really a {var2}.')
+
+# List Example:
+
+variable_1=['dog','cat','bird','guppy']
+
+variable_2=[
+    'Grey Wolf','Huge Tigger',
+    'Macaw Parrot','Great White Shark']
+
+variable_3=['John','Rob','Ron','Bob']
+
+variable_4=['Mom','Dad','Brother','Sister']
+
+variable_5=['friend','girlfriend','boyfriend','neighbour']
+
+for var1,var2,var3,var4,var5 in zip(variable_1,variable_2,variable_3,variable_4,variable_5):
+    print(f'My {var4} and my {var5} {var3} says my {var1} is really a {var2}.')
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # You can use Emojis in Python. I just discovered this on my own, just to see if
 # it would work. And it does work! WOW!! Use YouTube Emojis in your very own
 # Python program examples. Use them for creating logical programs that if
@@ -682,6 +718,64 @@ print(my_dictionary_list.get('key1')[0])
 print(my_dictionary_list.get('key2')[0])
 
 print(my_dictionary_list.get('key5','Sorry! Key Not Found:'))  # optional
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Let's create a dictionary that uses strings as keys and values, instead of
+# of actual text, like we did before. Let's create two, simple tuples; one for
+# the key tuple and one for the value tuple. We can also create them with or
+# without parentheses, but a '\' backslash must be implemented in place of the
+# parentheses. However, the Python programming standard shows only the
+# constant use of parentheses, not backslashes, as you can see in the next
+# example below.
+
+key='dog','cat','mouse','bird','fish' # tuple by default
+
+value=(
+    'Grey Wolf','Huge Tigger',
+    'Black Rat','Macaw Parrot',
+    'Great White Shark') # create a tuple with '()' parentheses.
+
+# Why use '()' parentheses when you can simply use the '\' backslash instead.
+# Note: '\' is not the usual Python programming standard, but it works. Now,
+# however, this only acts like a tuple by default, not a list as one would think.
+# You cannot change or modify tuple values at all; they are immutable, not
+# mutable like lists. Even though this works, it's not viable, especially when
+# you need to create a mutable list, not an immutable tuple, as this example
+# does by default. You must use either '()' parentheses for tuples, '[]' square
+# brackets for lists and '{}' curly braces for dictionaries and sets alike.
+
+key='dog','cat','mouse','bird','fish' # tuple by default
+
+value=\
+        'Grey Wolf','Huge Tigger',\
+        'Black Rat','Macaw Parrot',\
+        'Great White Shark' # tuple by default
+
+dictionary={ # dictionary
+    key[0]:value[0],
+    key[1]:value[1],
+    key[2]:value[2],
+    key[3]:value[3],
+    key[4]:value[4]
+    }
+
+# Non formatted examples with commas ',' and plus '+' signs
+
+for keys,values in dictionary.items():
+    print('My '+keys+' is really a '+values+'.')
+
+for keys,values in dictionary.items():
+    print('My',keys,'is really a',values+'.')
+
+# Old formatted example: now depreciated in Python 3 and up.
+# Can still be used in Python 3, thus far.
+
+for keys,values in dictionary.items():
+    print('My {} is really a {}.'.format(keys,values))
+
+# New formatted example: Python 3 and up.
+
+for keys,values in dictionary.items():
+    print(f'My {keys} is really a {values}.')
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # create a dictionary
 
@@ -1183,7 +1277,7 @@ my_cast = list(my_functions_set) # use the casting tuple function
 
 for i in my_cast:
     i()
-
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # I use def functions() most of the time. These are used for either
 # calling code through them, or they can be used as simple subroutines.
 # You can also use them within lists, which is what these Python
