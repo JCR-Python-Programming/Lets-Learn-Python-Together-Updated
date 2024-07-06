f25d7d5511210d7f5567c8f59f424271bdf60419
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2024-07-06 00:20:27 -0400

    Update Extensive Python Program Examples.py

--- Extensive Python Program Examples.py
+++ Extensive Python Program Examples.py
@@ -1255,6 +1255,24 @@ except ValueError:
 except MemoryError:
     print(words[4])
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Python sets{ } are great to use with numbers. Using numbers
+# in sets won't come out in random order, whereas using text
+# strings will always be in random order. Using numbers with
+# sets will always be in order, no matter where values are placed
+# within the set's parentheses.
+
+num_set = {9,1,2,3,5,4,6,7,8,0}
+
+print(num_set) # check your set values: optional
+
+# count the values in the set with the len( ) function
+
+print(len(num_set)) # there are ten values in the set, zero through nine
+
+add_num_values = sum(num_set) # add up all values in the set
+
+print(add_num_values) # added all ten values = 45
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 '''
 For those who are very new to Python programming or just programming
 at all. If you are getting into Python, you will often hear the word called:
@@ -3003,7 +3021,7 @@ class Grandmother:
     def __init__(self,fname,lname):
         self.first_name = fname
         self.last_name = lname
-        
+
     def grandmother(self):
         return 'I love you all!'
     def dog(self):
@@ -3012,7 +3030,7 @@ class Grandmother:
 class Mom(Grandmother):  # Mom class inherits Grandmother class
     def __init__(self,fname,lname):
         super().__init__(fname,lname)
-        
+
     def mom(self):
         return 'We love you two Mom!'
     def cat(self):
@@ -3021,7 +3039,7 @@ class Mom(Grandmother):  # Mom class inherits Grandmother class
 class Daughter(Mom):  # Daughter class inherits Mom class
     def __init__(self,fname,lname):
         super().__init__(fname,lname)
-        
+
     def daughter(self):
         return 'High Grandma!'
     def fish(self):
@@ -3030,7 +3048,7 @@ class Daughter(Mom):  # Daughter class inherits Mom class
 class Son(Daughter):   # Son class inherits Daughter class
     def __init__(self,fname,lname):
         super().__init__(fname,lname)
-        
+
     def son(self):
         return 'I love you Grandma!'
     def bird(self):
@@ -4477,3 +4495,121 @@ print(Attributes1('argument1','argument2','argument3').attribute1)
 print(Attributes2('argument1','argument2','argument3').attribute2)
 
 print(Attributes1.return_sum(1,2))
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+'''
+Python clock functions allow you to program the actual time in real time.
+Python clock functions work internally, in sync with the Windows clock.
+With Python clock functions; you can set the hour, minute, second, month,
+week, day and date. See Python clock function prefix descriptions below.
+
+'%I' 12-hour prefix
+'%H' 24-hour prefix
+'%M' Minutes prefix
+'%S' Seconds prefix
+'%p' AM/PM prefix
+'%A' Day of week prefix
+'%B' Month prefix
+'%d' Date prefix
+'%Y' Year prefix
+'%U' Weeks per year prefix
+'%j' Days per year prefix
+'''
+# Let's create a simple Python clock by invoking the Python clock function
+# prefixes. First, however, we also need to import two modules; 'time' and
+# 'datetime'. Type and execute/run the program example below and see
+# what happens.
+
+import time
+import datetime
+
+print(datetime.datetime.now().strftime('%I:%M:%S:%p'))
+print(datetime.datetime.now().strftime('%H:%M:%S'))
+print(datetime.datetime.now().strftime('%A %B %d,%Y'))
+print(datetime.datetime.now().strftime('Week %U'))
+print(datetime.datetime.now().strftime('Day %j'))
+
+# Remember you can reduce balky code via, using string variables. Let's
+# use 'timer' as the variable and use 'datetime.datetime.now()' as the value.
+# Type and execute/run the program example below and see what happens.
+
+import time
+import datetime
+
+timer=datetime.datetime.now()
+
+print(timer.strftime('%I:%M:%S:%p'))
+print(timer.strftime('%H:%M:%S'))
+print(timer.strftime('%A %B %d,%Y'))
+print(timer.strftime('Week %U'))
+print(timer.strftime('Day %j'))
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Now, let's create a tuple variable called 'show_time' so we can reduce
+# even more balky code, and also gain greater manipulative programming
+# skills at the same time. Type and execute/run the program example
+# below and see what happens.
+
+import time
+import datetime
+
+show_time=(
+    '%I:%M:%S:%p',
+    '%H:%M:%S',
+    '%A %B %d,%Y',
+    'Week %U',
+    'Day %j'
+    )
+
+timer=datetime.datetime.now()
+
+print(timer.strftime(show_time[0]))
+print(timer.strftime(show_time[1]))
+print(timer.strftime(show_time[2]))
+print(timer.strftime(show_time[3]))
+print(timer.strftime(show_time[4]))
+
+# Now change and rearrange the tuple number values [0] through [4] in
+# the program example above and re-execute/run the program and see
+# what happens.
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Now, let's make our Python clock come to life. Let's also keep the code
+# less balky and much more program manipulative at the same time. To
+# make the Python clock come to life, we are simply going to use a while-
+# loop to constantly refresh the screen output. A 'time.sleep()' function
+# will also be implemented to create a one-second sleep delay in the screen
+# output. Let's also implement the 'os.system()' function to clear the screen
+# output right after every one-second 'time.sleep' delay. Type and execute/
+# run the program example below and see what happens.
+
+import os
+import time
+import datetime
+
+show_time=(
+    '%I:%M:%S:%p',
+    '%H:%M:%S',
+    '%A %B %d,%Y',
+    'Week %U',
+    'Day %j'
+    )
+
+while True:
+    timer=datetime.datetime.now()
+    print(timer.strftime(show_time[0]))
+    print(timer.strftime(show_time[1]))
+    print(timer.strftime(show_time[2]))
+    print(timer.strftime(show_time[3]))
+    print(timer.strftime(show_time[4]))
+
+    time.sleep(1)
+    os.system('cls')
+
+# Let's shorten our code by reducing our print() functions down to only one,
+# using a for loop inside the while loop.
+
+while True:
+    for i in range(5):
+        timer=datetime.datetime.now()
+        print(timer.strftime(show_time[i]))
+
+    time.sleep(1)
+    os.system('cls')
