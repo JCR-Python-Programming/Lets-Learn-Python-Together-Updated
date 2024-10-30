2c4a8146201adfcfa69673e1ca1a03d0e82ebffb
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2024-10-30 15:32:05 -0400

    Update Iron Maiden Number of the Beast Lyrics.py

--- Iron Maiden Number of the Beast Lyrics.py
+++ Iron Maiden Number of the Beast Lyrics.py
@@ -12,49 +12,48 @@ Eddie = print
 
 IRON_MAIDEN_Number_of_The_Beast_lyrics = f'''
 
-esruoc sti ekat live ym ekam ot rewop eht evah I
-ecrof eht evah I ,erif eht evah I nrub uoy ekam ll'I
-dna ydob ruoy ssessop ll'I dnA nruter lliw I ,kcab
-gnimoc m'I
+...esruoc sti ekat live ym ekam ot rewop eht evah
+I .ecrof eht evah I ,erif eht evah I .nrub uoy ekam ll'I dna
+ydob ruoy ssessop ll'I dnA .nruter lliw I ,kcab gnimoc m'I
 
-em dna uoy rof eno eht ,{xof}
-tsaeb eht fo rebmun eht ,{xof}
+em dna uoy rof eno eht ,666
+tsaeb eht fo rebmun eht ,666
 
-seye rieht diova t'nac ,eziremsem ot meeS
-sedroh gnitnahc eht sdrawot nward leef I tuB
-?maerd yzarc emos tsuj ro ,laer eb llits siht naC
-wal eht mrofni tsum I ,no og t'nac sihT
+seye rieht diova
+t'nac ,eziremsem ot meeS .sedroh gnitnahc eht sdrawot
+nward leef I tuB ?maerd yzarc emos tsuj ro ,laer eb
+llits siht naC .wal eht mrofni tsum I ,no og t'nac sihT
 
 thginot no gniog si ecifircaS
-tsaeb eht fo rebmun eht ,{xof}
+tsaeb eht fo rebmun eht ,666
 
-enod si krow s'nataS ,nugeb sah lautir ehT
-thgirb gninrub era serif eht ,thgin eht nI yks eht
-ot dleh sdnah ,yrc ot trats yeht sA desiarp erew
-stnahc dercas dna dezalb sehcroT
+.enod
+si krow s'nataS ,nugeb sah lautir ehT .thgirb gninrub era
+serif eht ,thgin eht nI yks eht ot dleh sdnah ,yrc ot trats
+yeht sA .desiarp erew stnahc dercas dna dezalb sehcroT
 
-desaeler eb ot denwaps saw erif dna lleH
-tsaeb eht fo rebmun eht ,{xof}
+desaeler eb ot
+denwaps saw erif dna lleH tsaeb eht fo rebmun eht ,666
 
-?lleH fo dnik emos tsuj ro laer rof siht lla saW
-tsiwt dna evom serugif krad ,tsim eht nI ?em
-gnihctaw enoemos saw ,ees ot dah tsuj I esuaC'
-kcab gnidloh esu on saw ,kcalb saw thgiN
+?lleH fo dnik emos tsuj ro
+laer rof siht lla saW tsiwt dna evom serugif krad ,tsim
+eht nI ?em gnihctaw enoemos saw ,ees ot dah tsuj
+I esuaC' kcab gnidloh esu on saw ,kcalb saw thgiN
 
-riapsed ot em sgnirb dna dnim ym stsiwt taht ecaf
-live ehT ereht syawla s'ti smaerd ym ni esuaC' ?em
-ta kcab gnirats dnim depraw ym fo snoitcelfer yeht
-ereW smaerd dlo ym ni was I tahw tsuJ
+riapsed ot em sgnirb dna dnim
+ym stsiwt taht ecaf live ehT .ereht syawla s'ti smaerd
+ym ni esuaC' ?em ta kcab gnirats dnim depraw ym fo
+snoitcelfer yeht ereW .smaerd dlo ym ni was I tahw tsuJ
 
-?ysatnaf tsuj ton dna laer saw thgin taht was I tahw
-tahT ?eveileb I naC ?ees I did tahW dnim ym morf
-seiromem eht teg oT kniht ot emit dedeen I knalb
-saw dnim ym ,enola tfel I
+?ysatnaf tsuj ton dna laer saw
+thgin taht was I tahw tahT ?eveileb I naC ?ees I
+did tahW .dnim ym morf seiromem eht teg ot kniht
+ot emit dedeen I .knalb saw dnim ym ,enola tfel I
 
-{xof} si rebmun stI rebmun namuh a si ti roF tsaeb
-eht fo rebmun eht nokcer gnidnatsrednu htah ohw
-mih teL trohs si emit eht swonk eh esuaceB htarw
-htiw tsaeb eht sdnes liveD eht roF aes dna htrae ho
-,uoy ot eoW
+666 si rebmun
+stI rebmun namuh a si ti roF .tsaeb eht fo rebmun
+eht nokcer gnidnatsrednu htah ohw mih teL .trohs
+si emit eht swonk eh esuaceB .htarw htiw tsaeb eht
+sdnes liveD eht roF aes dna htrae ho ,uoy ot eoW
 '''
 Eddie(IRON_MAIDEN_Number_of_The_Beast_lyrics[::-1])
