cd7d1faef223c0bd922c4946fe40ec040eabbe89
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-02-25 22:35:43 -0500

    Add files via upload

---
+++ Integer Number Line.py
@@ -0,0 +1,22 @@
+# Hey kids, whenever a math teacher wants you to create
+# an integer number line, make Python cheat one up for
+# you in no time at all just by creating a simple for loop
+# that starts with a negative integer number that counts
+# backwards to zero, until it counts forward with positive
+# integer numbers, along with a step value of 1. After you
+# execute/run this Python program example, you can
+# highlight and copy, and then paste the contents of the
+# screen output into your math homework. You can create
+# a for loop that can count as many integer numbers you
+# please. You can also increase the step value to make
+# the integer numbers increment by two or whatever you
+# like. Experiment with this small Python program example.
+# Because some times, it's the small things that matter
+# the most.
+
+for i in range(-10,11,1):
+    print(i,end=', ')           # The end= prefix keeps print() function text on one line.
+
+# I am almost a complete Walking Human Computer Science Research
+# Laboratory Machine on Two Legs... 😁
+
