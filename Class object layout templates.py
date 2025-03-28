4b12adb2193f874dfa4ae26f2d74ca5637ccc08e
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-03-28 18:36:55 -0400

    Update Class object layout templates.py

--- Class object layout templates.py
+++ Class object layout templates.py
@@ -3,7 +3,7 @@
 # Created by Joseph C. Richardson, GitHub.com
 
 class Class_object_layout_template:
-    
+
     def __init__(self,value):
         self.value = value  # one attribute
 
@@ -22,7 +22,7 @@ class Class_object_layout_template:
     def __init__(self,value1,value2):
         self.value1 = value1
         self.value2 = value2  # two attributes
-        
+
 print(Class_object_layout_template('value1','value2').value1)
 print(Class_object_layout_template('value1','value2').value2)  # two attribute value extensions
 
@@ -44,7 +44,7 @@ class Class_object_layout_template:
         self.value1 = value1
         self.value2 = value2
         self.value3 = value3  # three attributes
-        
+
 print(Class_object_layout_template('value1','value2','value3').value1)
 print(Class_object_layout_template('value1','value2','value3').value2)
 print(Class_object_layout_template('value1','value2','value3').value3)  # three attribute value extensions
@@ -63,7 +63,7 @@ variable3 = Class_object_layout_template('value1','value2','value3')
 
 print(variable1.value1)
 print(variable2.value2)
-print(variable3.value3)  # three attribute value extensions                                    
+print(variable3.value3)  # three attribute value extensions
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 class Class_object_layout_template:
 
@@ -72,7 +72,7 @@ class Class_object_layout_template:
         self.value2 = value2
         self.value3 = value3
         self.value4 = value4  # four attributes
-        
+
 print(Class_object_layout_template('value1','value2','value3','value4').value1)
 print(Class_object_layout_template('value1','value2','value3','value4').value2)
 print(Class_object_layout_template('value1','value2','value3','value4').value3)
@@ -97,6 +97,81 @@ print(variable1.value1)
 print(variable2.value2)
 print(variable3.value3)
 print(variable4.value4)  # four attribute value extensions
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Super Class Object Layout Templates Python program examples:
+
+class Super_class_object_layout_template:
+
+    def __init__(self,value1):
+        self.value1 = value1  # one attribute
+
+class Super_class_object(Super_class_object_layout_template):
+
+    def __init__(self,value1,value2):
+        super().__init__(value1)
+
+        self.value2 = value2  # one unique attribute
+
+print(Super_class_object_layout_template('value1').value1)  # one attribute value extension
+
+print(Super_class_object('value1','value2').value1)  # one inherited attribute value extension
+
+print(Super_class_object('value1','value2').value2)  # one unique attribute value extension
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+class Super_class_object_layout_template:
+
+    def __init__(self,value1,value2):
+        self.value1 = value1
+        self.value2 = value2  # two attributes
+
+class Super_class_object(Super_class_object_layout_template):
+
+    def __init__(self,value1,value2,value3):
+        super().__init__(value1,value2)
+
+        self.value3 = value3  # one unique attribute
+
+print(Super_class_object_layout_template('value1','value2').value1)  # one attribute value extension
+
+print(Super_class_object_layout_template('value1','value2').value2)  # one attribute value extension
+
+
+print(Super_class_object('value1','value2','value3').value1)  # one inherited attribute value extension
+
+print(Super_class_object('value1','value2','value3').value2)  # one inherited attribute value extension
+
+
+print(Super_class_object('value1','value2','value3').value3)  # one unique attribute value extension
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+class Super_class_object_layout_template:
+
+    def __init__(self,value1,value2,value3):
+        self.value1 = value1
+        self.value2 = value2
+        self.value3 = value3  # three attributes
+
+class Super_class_object(Super_class_object_layout_template):
+
+    def __init__(self,value1,value2,value3,value4):
+        super().__init__(value1,value2,value3)
+
+        self.value4 = value4  # one unique attribute
+
+print(Super_class_object_layout_template('value1','value2','value3').value1)  # one attribute value extension
+
+print(Super_class_object_layout_template('value1','value2','value3').value2)  # one attribute value extension
+
+print(Super_class_object_layout_template('value1','value2','value3').value3)  # one attribute value extension
+
+
+print(Super_class_object('value1','value2','value3','value4').value1)  # one inherited attribute value extension
+
+print(Super_class_object('value1','value2','value3','value4').value2)  # one inherited attribute value extension
+
+print(Super_class_object('value1','value2','value3','value4').value3)  # one inherited attribute value extension
+
+
+print(Super_class_object('value1','value2','value3','value4').value4)  # one unique attribute value extension
 
 # I am almost a complete Walking Human Computer Science Research
 # Laboratory Machine on Two Legs... 😁
