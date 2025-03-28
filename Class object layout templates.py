f903cd553ff58ce320bd0f1077b859ccdcddb1f8
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-03-27 22:29:05 -0400

    Add files via upload

---
+++ Class object layout templates.py
@@ -0,0 +1,95 @@
+class Class_object_layout_template:
+    
+    def __init__(self,value):
+        self.value = value  # one attribute
+
+print(Class_object_layout_template('value').value)  # one attribute value extension
+
+variable = Class_object_layout_template('value').value  # one attribute value extension
+
+print(variable)
+
+variable = Class_object_layout_template('value')
+
+print(variable.value)  # one attribute value extension
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+class Class_object_layout_template:
+
+    def __init__(self,value1,value2):
+        self.value1 = value1
+        self.value2 = value2  # two attributes
+        
+print(Class_object_layout_template('value1','value2').value1)
+print(Class_object_layout_template('value1','value2').value2)  # two attribute value extensions
+
+variable1 = Class_object_layout_template('value1','value2').value1
+variable2 = Class_object_layout_template('value1','value2').value2  # two attribute value extensions
+
+print(variable1)
+print(variable2)
+
+variable1 = Class_object_layout_template('value1','value2')
+variable2 = Class_object_layout_template('value1','value2')
+
+print(variable1.value1)
+print(variable2.value2)  # two attribute value extensions
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+class Class_object_layout_template:
+
+    def __init__(self,value1,value2,value3):
+        self.value1 = value1
+        self.value2 = value2
+        self.value3 = value3  # three attributes
+        
+print(Class_object_layout_template('value1','value2','value3').value1)
+print(Class_object_layout_template('value1','value2','value3').value2)
+print(Class_object_layout_template('value1','value2','value3').value3)  # three attribute value extensions
+
+variable1 = Class_object_layout_template('value1','value2','value3').value1
+variable2 = Class_object_layout_template('value1','value2','value3').value2
+variable3 = Class_object_layout_template('value1','value2','value3').value3  # three attribute value extensions
+
+print(variable1)
+print(variable2)
+print(variable3)
+
+variable1 = Class_object_layout_template('value1','value2','value3')
+variable2 = Class_object_layout_template('value1','value2','value3')
+variable3 = Class_object_layout_template('value1','value2','value3')
+
+print(variable1.value1)
+print(variable2.value2)
+print(variable3.value3)  # three attribute value extensions                                    
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+class Class_object_layout_template:
+
+    def __init__(self,value1,value2,value3,value4):
+        self.value1 = value1
+        self.value2 = value2
+        self.value3 = value3
+        self.value4 = value4  # four attributes
+        
+print(Class_object_layout_template('value1','value2','value3','value4').value1)
+print(Class_object_layout_template('value1','value2','value3','value4').value2)
+print(Class_object_layout_template('value1','value2','value3','value4').value3)
+print(Class_object_layout_template('value1','value2','value3','value4').value4)  # four attribute value extensions
+
+variable1 = Class_object_layout_template('value1','value2','value3','value4').value1
+variable2 = Class_object_layout_template('value1','value2','value3','value4').value2
+variable3 = Class_object_layout_template('value1','value2','value3','value4').value3
+variable4 = Class_object_layout_template('value1','value2','value3','value4').value4  # four attribute value extensions
+
+print(variable1)
+print(variable2)
+print(variable3)
+print(variable4)
+
+variable1 = Class_object_layout_template('value1','value2','value3','value4')
+variable2 = Class_object_layout_template('value1','value2','value3','value4')
+variable3 = Class_object_layout_template('value1','value2','value3','value4')
+variable4 = Class_object_layout_template('value1','value2','value3','value4')
+
+print(variable1.value1)
+print(variable2.value2)
+print(variable3.value3)
+print(variable4.value4)  # four attribute value extensions
