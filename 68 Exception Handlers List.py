382e3185d469a4f603e57341678dc1df88121e2a
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-04-11 20:26:16 -0400

    Add files via upload

---
+++ 68 Exception Handlers List.py
@@ -0,0 +1,76 @@
+
+# Here is a complete alphabetical ordered list of 68 Exception Handlers in Python.
+
+# Created by Joseph C. Richardson, GitHub.com
+
+except ArithmeticError:
+except AssertionError:
+except AttributeError:
+except BaseException:
+except BlockingIOError:
+except BrokenPipeError:
+except BufferError:
+except BytesWarning:
+except ChildProcessError:
+except ConnectionAbortedError:
+except ConnectionError:
+except ConnectionRefusedError:
+except ConnectionResetError:
+except DeprecationWarning:
+except EOFError:
+except EnvironmentError:
+except Exception:  # Note: the Exception: handler will catch 'all' errors.
+except FileExistsError:
+except FileNotFoundError:
+except FloatingPointError:
+except FutureWarning:
+except GeneratorExit:
+except IOError:
+except ImportError:
+except ImportWarning:
+except IndentationError:
+except IndexError:
+except InterruptedError:
+except IsADirectoryError:
+except KeyError:
+except KeyboardInterrupt:
+except LookupError:
+except MemoryError:
+except ModuleNotFoundError:
+except NameError:
+except NotADirectoryError:
+except NotImplementedError:
+except OSError([arg]):
+except OSError(errno, strerror[, filename[, winerror[, filename2]]]):
+except OverflowError:
+except PendingDeprecationWarning:
+except PermissionError:
+except ProcessLookupError:
+except RecursionError:
+except ReferenceError:
+except ResourceWarning:
+except RuntimeError:
+except RuntimeWarning:
+except StopAsyncIteration:
+except StopIteration:
+except SyntaxError:
+except SyntaxWarning:
+except SystemError:
+except SystemExit:
+except TabError:
+except TimeoutError:
+except TypeError:
+except UnboundLocalError:
+except UnicodeDecodeError:
+except UnicodeEncodeError:
+except UnicodeError:
+except UnicodeTranslateError:
+except UnicodeWarning:
+except UserWarning:
+except ValueError:
+except Warning:
+except WindowsError:
+except ZeroDivisionError:
+
+# I am almost a complete Walking Human Computer Science Research
+# Laboratory Machine on Two Legs... 😁
