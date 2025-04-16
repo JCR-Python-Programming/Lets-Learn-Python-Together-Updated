0d9df3032ede96d0b0508d61fb8080baf7bca389
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-04-16 05:26:18 -0400

    Update TIMERNATOR Python Program.py

--- TIMERNATOR Python Program.py
+++ TIMERNATOR Python Program.py
@@ -8,50 +8,50 @@
 # the OS output screen, via double-clicking the Python
 # program file itself.
 
-# HIGHLIGHT AND COPY CODE, THEN PASTE INTO YOUR PREFERABLE PYTHON APP/IDLE
-
-import os,time,datetime,asyncio,winsound
+import os,datetime,asyncio,winsound
+from time import sleep as timernator
 
 os.system('title TIMERNATOR')
 
 text_colour=(
-    '\x1b[31m', # index 0 = red
-    '\x1b[32m', # index 1 = green
-    '\x1b[33m'  # index 2 = yellow
+    '\x1b[31m',  # index 0 = red
+    '\x1b[32m',  # index 1 = green
+    '\x1b[33m',  # index 2 = yellow
+    '\x1b[36m'   # index 3 = light blue
     )
 
 special_fx=(
     f'{text_colour[0]}TIMERNATOR', # index 0 = special_fx
 
-    'SPEECH OFF', # index 1 = special_fx
+    'Speech Off',  # index 1 = special_fx
 
-    'cls','\n','\n\n',' ' # index 2 = special_fx
+    'cls','\n','\n\n',' '  # index 2 = special_fx
     )
 
 time_fx=(
-f'{text_colour[1]}12 Hour {text_colour[0]}%I\
+f'{text_colour[2]}12 Hour {text_colour[0]}%I\
 {text_colour[1]}:{text_colour[0]}%M{text_colour[1]}\
-:{text_colour[0]}%S {text_colour[1]}%p', # index 0 = time_fx
+:{text_colour[0]}%S {text_colour[2]}%p',  # index 0 = time_fx
 
-f'{text_colour[1]}24 Hour {text_colour[0]}%H{text_colour[1]}:\
-{text_colour[0]}%M{text_colour[1]}:{text_colour[0]}%S', # index 1 = time_fx
+f'{text_colour[2]}24 Hour {text_colour[0]}%H{text_colour[1]}:\
+{text_colour[0]}%M{text_colour[1]}:{text_colour[0]}%S',  # index 1 = time_fx
 
 f'{text_colour[2]}%A %B {text_colour[0]}%d{text_colour[1]}:\
 {text_colour[0]}%Y',f'{text_colour[2]}Week{text_colour[1]}:\
 {text_colour[0]}%U {text_colour[2]}Day{text_colour[1]}:\
-{text_colour[0]}%j' # index 2 = time_fx
+{text_colour[0]}%j'  # index 2 = time_fx
     )
 
 text_fx=(
-    f'{text_colour[2]}You\'re TIMERNATED!', # index 0 = text_fx
+    f'{text_colour[3]}You\'re TIMERNATED!',  # index 0 = text_fx
 
-    f'{text_colour[2]}Look at me if you want the time.', # index 1 = text_fx
+    f'{text_colour[3]}Look at me if you want the time.',  # index 1 = text_fx
 
-    f'{text_colour[2]}I need your hours, your minutes and your seconds.', # index 2 = text_fx
+    f'{text_colour[3]}I need your hours, your minutes and your seconds.',  # index 2 = text_fx
 
-    f'{text_colour[2]}I swear I will tell the time.', # index 3 = text_fx
+    f'{text_colour[3]}I swear I will tell the time.',  # index 3 = text_fx
 
-    f'{text_colour[2]}I cannot self timernate.' # index 4 = text_fx
+    f'{text_colour[3]}I cannot self timernate.'  # index 4 = text_fx
     )
 
 redundant_code1='''
@@ -84,7 +84,7 @@ while True:
             timer=datetime.datetime.now()
             exec(redundant_code2)
 
-        time.sleep(1)
+        timernator(1)
 
         os.system(special_fx[2])
 
@@ -98,7 +98,7 @@ while True:
             timer=datetime.datetime.now()
             exec(redundant_code2)
 
-        time.sleep(1)
+        timernator(1)
 
         os.system(special_fx[2])
 
@@ -112,4 +112,7 @@ while True:
             timer=datetime.datetime.now()
             exec(redundant_code2)
 
-        time.sleep(1)
+        timernator(1)
+
+# I am almost a complete Walking Human Computer Science Research
+# Laboratory Machine on Two Legs... 😁
