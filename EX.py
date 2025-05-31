1b14c1ec316dbcd01593f3d165c1dff73d5f76bc
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-05-31 19:25:15 -0400

    Add files via upload

---
+++ EX.py
@@ -0,0 +1,130 @@
+
+def function():
+  print('This define function prints text only.')
+
+function()
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def function_argument(argument):
+  print('This define function retrieves one argument',argument)
+
+function_argument('value.')
+
+
+def function_argument(argument):
+  print('This define function retrieves one argument '+argument)
+
+function_argument('value.')
+
+
+def function_argument(argument):
+  print('This define function retrieves one argument {}'.format(argument))
+
+function_argument('value.')
+
+
+def function_argument(argument):
+  print(f'This define function retrieves one argument {argument}')
+
+function_argument('value.')
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def function_argument(argument1,argument2):
+  print(f'This define function retrieves two {argument1} {argument2}')
+
+function_argument('argument','values.')
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def function_argument(argument1,argument2,argument3):
+  print(f'This define {argument1} retrieves three {argument2} {argument3}')
+
+function_argument('function','argument','values.')
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def return_function(argument):
+  return 'value.'
+
+print('This define function returns one argument',return_function('argument placeholder value'))
+
+
+def return_function(argument):
+  return 'value.'
+
+print('This define function returns one argument '+return_function('argument placeholder value'))
+
+
+def return_function(argument):
+  return 'value.'
+
+print('This define function returns one argument {} '.format(return_function('argument placeholder value')))
+
+
+def return_function(argument):
+  return 'value.'
+
+print(f'This define function returns one argument {return_function("argument placeholder value")}')
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def return_function(argument1,argument2):
+  return 'argument','values.'
+
+print(f'This define function returns two \
+{return_function("argument placeholder value","argument placeholder value")[0]} \
+{return_function("argument placeholder value","argument placeholder value")[1]}')
+
+
+def return_function(*args):
+  return 'argument','values.'
+
+print(f'This define function returns two \
+{return_function("argument placeholder value","argument placeholder value")[0]} \
+{return_function("argument placeholder value","argument placeholder value")[1]}')
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def return_function(argument1,argument2,argument3):
+  return 'argument','values.','WOW!'
+
+print(f'This define function returns three \
+{return_function("argument placeholder value","argument placeholder value","argument placeholder value")[0]} \
+{return_function("argument placeholder value","argument placeholder value","argument placeholder value")[1]} \
+{return_function("argument placeholder value","argument placeholder value","argument placeholder value")[2]}')
+
+
+def return_function(*args):
+  return 'argument','values.','WOW!'
+
+print(f'This define function returns three \
+{return_function("argument placeholder value","argument placeholder value","argument placeholder value")[0]} \
+{return_function("argument placeholder value","argument placeholder value","argument placeholder value")[1]} \
+{return_function("argument placeholder value","argument placeholder value","argument placeholder value")[2]}')
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def return_function(num1,num2):
+  return num1+num2
+
+print(return_function(5,4))
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def return_function(num1,num2):
+  return num1-num2
+
+print(return_function(5,4))
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def return_function(num1,num2):
+  return num1*num2
+
+print(return_function(5,4))
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def return_function(num1,num2):
+  return num1/num2
+
+print(return_function(5,4))
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def return_function(num1,num2):
+  return num1**num2
+
+print(return_function(5,4))
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def return_function(num1,num2,num3,num4,num5,num6):
+  return num1+num2-num3*num4/num5**num6
+
+print(return_function(5,4,3,6,3,2))
+
+
+def return_function(*args):
+  return args[0]+args[1]-args[2]*args[3]/args[4]**args[5]
+
+print(return_function(5,4,3,6,3,2))
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
