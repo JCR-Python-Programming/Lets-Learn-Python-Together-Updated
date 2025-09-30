9c11fef55609cb584d22d4ffb82a871f04f7fa51
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-09-30 12:55:49 -0400

    Add files via upload

--- Know Your Polygons.py
+++ Know Your Polygons.py
@@ -1,71 +1,65 @@
-# Know Your Polygons
+# Know Your Polygons game Python program example:
 
-# Created by Joseph C. Richardson, GitHub.com
+# See what happens when you type and execute/run this guessing game program
+# example below. Note: you must execute/run the program from the OS output screen,
+# via double-clicking the Python program file itself.
 
-# HIGHLIGHT AND COPY CODE, THEN PASTE INTO YOUR PREFERABLE PYTHON APP/IDLE
-
-# See what happens when you type and execute/run this guessing
-# game program example below. Note: you must execute/run the
-# program from the OS output screen, via double-clicking the Python
-# program file itself.
-
-# Save the Python file as 'Know Your Polygons'
+# Save the Python file as 'Know Your Polygons.py'
 
 import os
 
-tc=(
-    '\x1b[31m',
-    '\x1b[32m',
-    '\x1b[33m',
-    '\x1b[34m',
-    '\x1b[35m',
-    '\x1b[36m',
-    '\x1b[37m',
-    'cls'
-    )
+text_col = (
+  '\x1b[31m',  # index 0 = red
+  '\x1b[32m',  # index 1 = green
+  '\x1b[33m',  # index 2 = yellow
+  '\x1b[34m',  # index 3 = blue
+  '\x1b[35m',  # index 4 = purple
+  '\x1b[36m',  # index 5 = cyan
+  '\x1b[37m',  # index 6 = white
+  'cls')  # index 7 = clear screen
 
-question_prompts1=(
-    f'{tc[2]}How many sides does a Triangle have?\n\n{tc[1]}(a) {tc[2]}four sides\n\
-{tc[1]}(b) {tc[2]}three sides\n{tc[1]}(c) {tc[2]}two sides',
-    
-    f'{tc[2]}How many sides does a Square have?\n\n{tc[1]}(a) {tc[2]}Two sides\n\
-{tc[1]}(b) {tc[2]}Three sides\n{tc[1]}(c) {tc[2]}Four sides',
-    
-    f'{tc[2]}How many sides does a Pentagon have?\n\n{tc[1]}(a) {tc[2]}four sides\n\
-{tc[1]}(b) {tc[2]}five sides\n{tc[1]}(c) {tc[2]}Three sides',
-    
-    f'{tc[2]}How many sides does a Hexagon have?\n\n{tc[1]}(a) {tc[2]}six sides\n\
-{tc[1]}(b) {tc[2]}five sides\n{tc[1]}(c) {tc[2]}two sides',
-    
-    f'{tc[2]}How many sides does a Octagon have?\n\n{tc[1]}(a) {tc[2]}four sides\n\
-{tc[1]}(b) {tc[2]}six sides\n{tc[1]}(c) {tc[2]}eight sides',
-    
-    f'{tc[2]}How many sides does a Dodecagon have?\n\n{tc[1]}(a) {tc[2]}eight \
-sides\n{tc[1]}(b) {tc[2]}three sides\n{tc[1]}(c) {tc[2]}twelve sides',
-    
-    f'{tc[2]}How many sides does a Hexadecagon have?\n\n{tc[1]}(a) {tc[2]}sixteen \
-sides\n{tc[1]}(b) {tc[2]}eight sides\n{tc[1]}(c) {tc[2]}six sides'
-    )
+question_prompts = (
+  f'{text_col[2]}How many sides does a Triangle have?\n\n{text_col[1]}(a) {text_col[2]}four sides\
+\n{text_col[1]}(b) {text_col[2]}three sides\n{text_col[1]}(c) {text_col[2]}two sides',
 
-prompt=('b','c','b','a','c','c','a')
+  f'{text_col[2]}How many sides does a Square have?\n\n{text_col[1]}(a) {text_col[2]}Two sides\
+\n{text_col[1]}(b) {text_col[2]}Three sides\n{text_col[1]}(c) {text_col[2]}Four sides',
 
-score=0
-loop=0
+  f'{text_col[2]}How many sides does a Pentagon have?\n\n{text_col[1]}(a) {text_col[2]}four sides\
+\n{text_col[1]}(b) {text_col[2]}five sides\n{text_col[1]}(c) {text_col[2]}Three sides',
 
-while loop<=6:
-    
-    os.system(tc[7])
-    button=input((tc[1])+'\nKnow Your Polygons!\n\n'+(tc[2])+'Know Your Polygons\n\n'+\
-    question_prompts1[loop]+'\n\n'+(tc[0])+'READY:'+(tc[1])).strip()
-    
-    if button==(prompt[loop]):
-        score+=1
-        
-    loop+=1
-    
-    os.system(tc[7])    
-print(f'\n{tc[2]}Know Your Polygons\n\n{tc[2]}You got \
-{score}/{len(question_prompts1)} questions correct.\nCongratulations! Your total \
-Prize Winnings: {tc[1]}${score*100*score:,}.00 {tc[2]}Dollars.\n\n{tc[0]}READY:')
+  f'{text_col[2]}How many sides does a Hexagon have?\n\n{text_col[1]}(a) {text_col[2]}six sides\
+\n{text_col[1]}(b) {text_col[2]}five sides\n{text_col[1]}(c) {text_col[2]}two sides',
 
-input('\nEND OF PROGRAM! Press Enter to quit.')
+  f'{text_col[2]}How many sides does a Octagon have?\n\n{text_col[1]}(a) {text_col[2]}four sides\
+\n{text_col[1]}(b) {text_col[2]}six sides\n{text_col[1]}(c) {text_col[2]}eight sides',
+
+  f'{text_col[2]}How many sides does a Dodecagon have?\n\n{text_col[1]}(a) {text_col[2]}eight\
+sides\n{text_col[1]}(b) {text_col[2]}three sides\n{text_col[1]}(c) {text_col[2]}twelve sides',
+
+  f'{text_col[2]}How many sides does a Hexadecagon have?\n\n{text_col[1]}(a) {text_col[2]}sixteen\
+sides\n{text_col[1]}(b) {text_col[2]}eight sides\n{text_col[1]}(c) {text_col[2]}six sides')
+
+prompt = ('b','c','b','a','c','c','a')
+
+score = 0
+loop = 0
+
+while loop <= 6:
+
+  os.system(text_col[7])
+  button = input((text_col[1])+'\nKnow Your Polygons!\n\n'+(text_col[2])+'Know Your Polygons\n\n'+\
+  question_prompts[loop]+'\n\n'+(text_col[0])+'READY: '+(text_col[1])).strip()
+
+  if button == (prompt[loop]):
+      score += 1
+
+  loop += 1
+
+  os.system(text_col[7])
+
+print(f'\n{text_col[2]}Know Your Polygons\n\n{text_col[2]}You got \
+{score}/{len(question_prompts)} questions correct.\nCongratulations! Your total \
+Prize Winnings: {text_col[1]}${score*100*score:,}.00 {text_col[2]}Dollars.')
+
+input(f'\n{text_col[5]}Press Enter to exit:')
