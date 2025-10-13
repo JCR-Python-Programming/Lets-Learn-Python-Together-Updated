52ab1a55407339e98c35db9a876ffa2f2c67e56d
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-10-13 01:02:09 -0400

    Add files via upload

--- Skeletal Class Acts Two.py
+++ Skeletal Class Acts Two.py
@@ -21,15 +21,18 @@ class Main:
     self.two = two
     self.three = three
 
+  def return_function(self):
+    return 1,2,3,4
+
 class Sub_super_one(Main):
   def __init__(self,one,two,three):
     super().__init__(one,two,three)
 
-class Sub_super_two(Sub_super_one):
+class Sub_super_two(Main):
   def __init__(self,one,two,three):
     super().__init__(one,two,three)
 
-class Sub_super_three(Sub_super_two):
+class Sub_super_three(Main):
   def __init__(self,one,two,three,four):
     super().__init__(one,two,three)
 
@@ -46,4 +49,14 @@ print(Sub_super_two('One','Two','Three').three)
 print(Sub_super_three('One','Two','Three','four').four)
 
 print(Child('One','Two','Three','four').four)
+
+# Return function calls:
+
+print(Main.return_function('return argument placeholder value')[0])
+
+print(Sub_super_one.return_function('return argument placeholder value')[1])
+
+print(Sub_super_two.return_function('return argument placeholder value')[2])
+
+print(Child.return_function('return argument placeholder value')[3])
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
