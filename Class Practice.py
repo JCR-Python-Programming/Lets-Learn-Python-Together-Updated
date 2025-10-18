66f099ccb628f7bd938926118594f279c043320a
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-10-18 10:46:58 -0400

    Add files via upload

--- Class Practice.py
+++ Class Practice.py
@@ -51,7 +51,12 @@ class Super_sub_extra_attribute(Polygons):
     self.material = material
 
 class Child_class_all(Super_sub_extra_attribute,Super_sub_same_attributes,Polygons):
-  pass
+
+  def Child_class_return_value():
+    return 'Yay! We did it!!'
+
+  def Child_class_return_addition(num1,num2):
+    return num1 + num2
 
 # Class values:
 
@@ -92,3 +97,102 @@ print(Child_class_all(polygon,sides,colour,material).sides[0])
 print(Child_class_all(polygon,sides,colour,material).colour[0])
 
 print(Child_class_all(polygon,sides,colour,material).material[0])
+
+# Define function returned child class value:
+
+print(Child_class_all.Child_class_return_value())
+
+# Define function returned arguments child class values:
+
+print(Child_class_all.Child_class_return_addition(2,3))  # 2 + 3 = 5
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+class Math_class:
+
+  def __init__(self,addition,subtraction,multiplication,division,exponent):
+
+    self.addition = addition
+    self.subtraction = subtraction
+    self.multiplication = multiplication
+    self.division = division
+    self.exponent = exponent
+
+  def addition(num1,num2):
+      return num1 + num2
+
+  def subtraction(num1,num2):
+      return num1 - num2
+
+  def multiplication(num1,num2):
+      return num1 * num2
+
+  def division(num1,num2):
+      return num1 / num2
+
+  def exponent(num1,num2):
+      return num1 ** num2
+
+class Super_sub_same_attributes(Math_class):
+
+  def __init__(self,addition,subtraction,multiplication,division,exponent):
+    super().__init__(addition,subtraction,multiplication,division,exponent)
+
+class Super_sub_extra_attribute(Math_class):
+
+  def __init__(self,addition,subtraction,multiplication,division,exponent,professor):
+    super().__init__(addition,subtraction,multiplication,division,exponent)
+
+    self.professor = professor
+
+professors = 'Galileo Galilei','Isaac Newton','Albert Einstein','Stephen Hawking'
+
+a,b = 8,2
+
+# Define function returned class with two argument values:
+
+print(Math_class.addition(a,b))
+
+print(Math_class.subtraction(a,b))
+
+print(Math_class.multiplication(a,b))
+
+print(int(Math_class.division(a,b)))
+
+print(Math_class.exponent(a,b))
+
+# Super sub one class with two argument values:
+
+print(Super_sub_same_attributes.addition(a,b))
+
+print(Super_sub_same_attributes.subtraction(a,b))
+
+print(Super_sub_same_attributes.multiplication(a,b))
+
+print(int(Super_sub_same_attributes.division(a,b)))
+
+print(Super_sub_same_attributes.exponent(a,b))
+
+# Super sub two class with two argument values:
+
+print(Super_sub_extra_attribute.addition(a,b))
+
+print(Super_sub_extra_attribute.subtraction(a,b))
+
+print(Super_sub_extra_attribute.multiplication(a,b))
+
+print(int(Super_sub_extra_attribute.division(a,b)))
+
+print(Super_sub_extra_attribute.exponent(a,b))
+
+print(Super_sub_extra_attribute(a+b,a-b,a*b,a/b,a**b,professors).professor[0])
+
+# Class values:
+
+print(Math_class(a+b,a-b,a*b,a/b,a**b).addition)
+
+print(Math_class(a+b,a-b,a*b,a/b,a**b).subtraction)
+
+print(Math_class(a+b,a-b,a*b,a/b,a**b).multiplication)
+
+print(int(Math_class(a+b,a-b,a*b,a/b,a**b).division))
+
+print(Math_class(a+b,a-b,a*b,a/b,a**b).exponent)
