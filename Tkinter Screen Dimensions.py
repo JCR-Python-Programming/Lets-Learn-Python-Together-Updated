9d508e18daa7c402eb660a30397971a7477117d8
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-11-17 18:06:09 -0500

    Add files via upload

--- Tkinter Screen Dimensions.py
+++ Tkinter Screen Dimensions.py
@@ -17,11 +17,11 @@ cyan = '#00ffff'
 
 w,h = 1365,744
 r,c = 0,0
-rand1,rand2 = 1,20
-randint1,randint2 = -1365,1365
+line_width1,line_width2 = 1,20
+screen_width1,screen_width2 = -1365,1365
 increase_decrease_screen_objects = 20
-blink_rate = .11
-screensaver_time_cycle = 30
+blink_rate = .12
+screensaver_time_cycle = 200
 everything = 'all'
 
 screensaver.title('Tkinter Screen Dimensions')
