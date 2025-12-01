bc6250797d7d65b2ad638352de91c0c6552f22bc
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-12-01 10:57:50 -0500

    Add files via upload

--- Class Input Return Functions.py
+++ Class Input Return Functions.py
@@ -1,6 +1,3 @@
-user_input1 = input('Please type a number, or letter key, then press enter: ')
-user_input2 = input('Please type second number, or letter key, then press enter: ')
-  
 class Input_text:
 
   def __init__(self,user_input1,user_input2):
@@ -12,12 +9,12 @@ class Input_text:
   def return_user_input(self):
     return f'{self.user_input1} {self.user_input2}'
 
+user_input1 = input('Please type a number, or letter key, then press enter: ')
+user_input2 = input('Please type second number, or letter key, then press enter: ')
+
 print(Input_text(user_input1,user_input2).user_input1)
 print(Input_text(user_input1,user_input2).user_input2)
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-user_input1 = input('Please type a number, or letter key, then press enter: ')
-user_input2 = input('Please type second number, or letter key, then press enter: ')
-  
 class Input_text:
 
   def __init__(self,user_input1,user_input2):
@@ -28,12 +25,12 @@ class Input_text:
 
   def return_user_input(self):
     return f'{self.user_input1}\n{self.user_input2}'
+
+user_input1 = input('Please type a number, or letter key, then press enter: ')
+user_input2 = input('Please type second number, or letter key, then press enter: ')
 
 print(Input_text(user_input1,user_input2).return_user_input())
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-user_input1 = input('Please type a number, or letter key, then press enter: ')
-user_input2 = input('Please type second number, or letter key, then press enter: ')
-  
 class Input_text:
 
   def __init__(self,user_input1,user_input2):
@@ -44,6 +41,9 @@ class Input_text:
 
   def return_user_input(self):
     return f'{self.user_input1}\n{self.user_input2}'
+
+user_input1 = input('Please type a number, or letter key, then press enter: ')
+user_input2 = input('Please type second number, or letter key, then press enter: ')
 
 print(Input_text(user_input1,user_input2).user_input1)
 print(Input_text(user_input1,user_input2).user_input2)
