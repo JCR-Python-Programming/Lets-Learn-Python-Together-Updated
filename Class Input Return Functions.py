a400de64fb36469f247a7de2f919b3939d3f19fa
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-12-01 11:45:30 -0500

    Add files via upload

--- Class Input Return Functions.py
+++ Class Input Return Functions.py
@@ -49,3 +49,35 @@ print(Input_text(user_input1,user_input2).user_input1)
 print(Input_text(user_input1,user_input2).user_input2)
 
 print(Input_text(user_input1,user_input2).return_user_input())
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+class Input_text:
+
+  def __init__(self,user_input1,user_input2):
+
+    self.user_input1 = user_input1  # attribute1 property:
+
+    self.user_input2 = user_input2  # attribute2 property:
+
+  def return_user_input(self):
+    return self.user_input1+'\n'+self.user_input2
+
+user_input1 = input('Please type a number, or letter key, then press enter: ')
+user_input2 = input('Please type second number, or letter key, then press enter: ')
+
+print(Input_text(user_input1,user_input2).return_user_input())
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+class Input_text:
+
+  def __init__(self,user_input1,user_input2):
+
+    self.user_input1 = user_input1  # attribute1 property:
+
+    self.user_input2 = user_input2  # attribute2 property:
+
+  def return_user_input(self):
+    return '{}\n{}'.format(self.user_input1,self.user_input2)
+
+user_input1 = input('Please type a number, or letter key, then press enter: ')
+user_input2 = input('Please type second number, or letter key, then press enter: ')
+
+print(Input_text(user_input1,user_input2).return_user_input())
