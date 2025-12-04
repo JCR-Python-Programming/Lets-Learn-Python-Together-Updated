fe67c040ee4c23d2c6eaa0a0cc3ea2cc9e5bfbb6
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-12-04 12:24:34 -0500

    Add files via upload

---
+++ Class Input Return Math Functions IV.py
@@ -0,0 +1,48 @@
+import math
+
+class Math:
+  def __init__(self,user_input1,user_input2):
+
+    self.user_input1 = user_input1  # attribute1 property:
+
+    self.user_input2 = user_input2  # attribute2 property:
+
+  def return_answer(self):
+    return (
+      f'{self.user_input1+self.user_input2}\n\
+{self.user_input1-self.user_input2}\n\
+{self.user_input1*self.user_input2}\n\
+{self.user_input1/self.user_input2}\n\
+{self.user_input1**self.user_input2}\n\
+{pow(self.user_input1,self.user_input2)}\n\
+{math.sqrt(self.user_input2)}')
+
+class Great_work(Math):
+
+  def __init__(self,user_input1,user_input2,user_input3):
+    super().__init__(user_input1,user_input2)
+
+    self.user_input3 = user_input3  # super attribute3 property:
+
+  def return_super(self):
+    return self.user_input3
+
+message = (
+  'Please type a number, the press enter: ',
+  'Please type a second number, then press enter: ',
+  'Numbers only please:','Great Work!!')
+
+while True:
+
+  try:
+    user_input1 = int(input(message[0]).strip())
+    user_input2 = int(input(message[1]).strip())
+    break
+
+  except ValueError:
+    print(message[2])
+
+answer = Math(user_input1,user_input2).return_answer()
+great_work = Great_work(user_input1,user_input2,message[3]).return_super()
+
+print(f'{answer}\n{great_work}')
