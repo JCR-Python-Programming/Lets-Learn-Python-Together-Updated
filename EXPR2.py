2e642281aa061ccefa256081df4618d3099584ee
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-12-22 07:00:09 -0500

    Add files via upload

--- EXPR2.py
+++ EXPR2.py
@@ -1,8 +1,8 @@
 
 class Main:
-    def __init__(self,*args):
-
-        self.args = args
+  def __init__(self,*args):
+    
+    self.args = args
 
 class sub(Main):pass
 
@@ -10,74 +10,73 @@ print(Main('argument1','argument2','argument3').args[0])
 
 print(sub('argument1','argument2','argument3').args[1])
 
+
 class Main:
-    def __init__(self,**kwargs):
+  def __init__(self,**kwargs):
 
-        self.kwargs = kwargs
-
+    self.kwargs = kwargs
 
 class sub(Main):pass
 
-lazy1 = Main(kw_arg1 = 'argument1',kw_arg2 = 'argument2',kw_arg3 = 'argument3').kwargs.get('kw_arg4','Attribute does not exist') # None
+lazy1 = Main(kw_arg1 = 'argument1',kw_arg2 = 'argument2',kw_arg3 = 'argument3')
 
-print(lazy1)
+print(lazy1.kwargs.get('kw_arg4','Attribute does not exist')) # None
+
+input()
+
 
 class Main:
-    def __init__(self,**kwargs):
+  def __init__(self,**kwargs):
 
-        self.__dict__.update(kwargs)
+    self.__dict__.update(kwargs)
 
 class sub(Main):pass
 
-lazy2 = Main(kw_arg1 = 'argument1',kw_arg2 = 'argument2',kw_arg3 = 'argument3')
+lazy = Main(kw_arg1 = 'argument1',kw_arg2 = 'argument2',kw_arg3 = 'argument3')
 
-print(lazy2.kw_arg1)
+print(lazy.kw_arg1)
+
 
 class Main:
+  def __init__(self,par1,par2,par3):
 
-    def __init__(self,par1,par2,par3):
+    self.parameter1 = par1
+    self.parameter2 = par2
+    self.parameter3 = par3
 
-        self.parameter1 = par1
-        self.parameter2 = par2
-        self.parameter3 = par3
-
-    def Testing1():
-        print('Testing1, Testing1')
+  def Testing1():
+      print('Testing1, Testing1')
 
 class Super1(Main):
+  def __init__(self,parameter1,parameter2,parameter3,par4):
+    super().__init__(parameter1,parameter2,parameter3)
 
-    def __init__(self,parameter1,parameter2,parameter3,par4):
-        super().__init__(parameter1,parameter2,parameter3)
-
-        self.parameter4 = par4
+    self.parameter4 = par4
 
 class Super2(Super1):
+  def __init__(self,parameter1,parameter2,parameter3,parameter4,par5):
+    super().__init__(parameter1,parameter2,parameter3,parameter4)
 
-    def __init__(self,parameter1,parameter2,parameter3,parameter4,par5):
-        super().__init__(parameter1,parameter2,parameter3,parameter4)
-
-        self.parameter5 = par5
+    self.parameter5 = par5
 
 class Super3(Super2):
+  def __init__(self,parameter1,parameter2,parameter3,parameter4,parameter5,par6):
+      super().__init__(parameter1,parameter2,parameter3,parameter4,parameter5)
 
-    def __init__(self,parameter1,parameter2,parameter3,parameter4,parameter5,par6):
-        super().__init__(parameter1,parameter2,parameter3,parameter4,parameter5)
-
-        self.parameter6 = par6
+      self.parameter6 = par6
 
 class Super4(Super3):
+  def __init__(self,parameter1,parameter2,parameter3,parameter4,parameter5,parameter6,par7):
+    super().__init__(parameter1,parameter2,parameter3,parameter4,parameter5,parameter6)
 
-    def __init__(self,parameter1,parameter2,parameter3,parameter4,parameter5,parameter6,par7):
-        super().__init__(parameter1,parameter2,parameter3,parameter4,parameter5,parameter6)
-
-        self.parameter7 = par7
+    self.parameter7 = par7
 
 class Sub:
-    def Testing2():
-        print('Testing2, Testing2')
+  def Testing2():
+    print('Testing2, Testing2')
 
 class Child(Sub,Main):
-    pass
+  pass
 
 text1 = Main('parameter1','parameter2','parameter3').parameter3
 
