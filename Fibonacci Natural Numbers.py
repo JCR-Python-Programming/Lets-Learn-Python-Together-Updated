7ccd1e85490b04700b805e409afdfbe2c59cbc42
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-12-27 23:22:55 -0500

    Add files via upload

--- Fibonacci Natural Numbers.py
+++ Fibonacci Natural Numbers.py
@@ -8,61 +8,62 @@
 
 # HIGHLIGHT AND COPY CODE, THEN PASTE INTO YOUR PREFERABLE PYTHON APP/IDLE
 
-import os,time,winsound
+import os;os.system(f'title Fibonacci Natural Number Sequence')
+import time
+import atexit
 
-os.system(f'title Fibonacci Natural Number Sequence')
+def restore_terminal():
+  print(show_cursor,flush = True)
+
+atexit.register(restore_terminal)
 
 text_colours=(
-    '\x1b[31m', # index 0 = red
-    '\x1b[32m', # index 1 = green
-    '\x1b[33m', # index 2 = yellow
-    '\x1b[34m', # index 3 = blue
-    '\x1b[35m', # index 4 = purple
-    '\x1b[36m', # index 5 = cyan
-    '\x1b[37m'  # index 6 = white
+  '\033[38;2;255;0;255m', # index 0 = red
+  '\033[38;2;0;255;0m', # index 1 = green
+  '\033[38;2;255;255;0m', # index 2 = yellow
+  '\033[38;2;0;255;255m', # index 3 = cyan
     )
 
-text_words=(
-    f'{text_colours[1]}Fibonacci Natural Number Sequence in Action...',
+anti_flick = '\033[H'+'\033[?25l'
 
-    f'\n\n{text_colours[2]}Fibonacci Natural Number Sequence example: \
+# show_cursor = '\033[?25h\033[0m'
+
+text_words=(
+  f'{text_colours[1]}Fibonacci Natural Number Sequence in Action...',
+
+  f'\n\n{text_colours[2]}Fibonacci Natural Number Sequence example: \
 [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610...]',
 
-    f'\n\n{text_colours[5]}Fibonacci Natural Numbers go on forever!',
-    f'\n\nFibonacci Natural Numbers can only be found in \
+  f'\n\n{text_colours[3]}Fibonacci Natural Numbers go on forever!',
+  f'\n\nFibonacci Natural Numbers can only be found in \
 nature, such as plants and animals...'
     )
 
-sounds=('Ring03','Speech Off')
-
 def Fib_Num():
-    winsound.PlaySound(sounds[0],winsound.SND_ASYNC)
 
-    for i in range(25):
-        print('\n',' '*i,text_words[0])
-        time.sleep(.25)
-        os.system('cls')
+  for i in range(25):
+    print(anti_flick+'\n',' '*i,text_words[0])
+    time.sleep(.10)
 
-    num1=0
-    num2=1
-    fib=[num1,num2]
+  num1=0
+  num2=1
+  fib=[num1,num2]
 
-    while True:
-        num3=num1+num2
-        fib.append(num3)
-        num1=num2
-        num2=num3
-        clock=(time.asctime())
+  while True:
+    num3=num1+num2
+    fib.append(num3)
+    num1=num2
+    num2=num3
+    clock=(time.asctime())
 
-        print('\n',' '*25,text_words[0],text_words[1],text_words[2])
+    print(anti_flick+'\n',' '*25,text_words[0],text_words[1],text_words[2])
 
-        print(f'\nFibonacci Natural Number Sequence: {text_colours[2]}\
-{num1} {text_colours[5]}+ {text_colours[2]}{num2}{text_colours[5]} = \
-({text_colours[0]}{num1+num3}{text_colours[5]}){text_colours[5]}\n\n\
-Fibonacci Natural Numbers: "{text_colours[0]}{num1+num3:,}{text_colours[5]}\
+    print(f'\nFibonacci Natural Number Sequence: {text_colours[2]}\
+{num1} {text_colours[3]}+ {text_colours[2]}{num2}{text_colours[3]} = \
+({text_colours[0]}{num1+num3}{text_colours[3]}){text_colours[3]}\n\n\
+Fibonacci Natural Numbers: "{text_colours[0]}{num1+num3:,}{text_colours[3]}\
 "\n\n{text_colours[0]}Date & Time:\n\n{clock}')
 
-        winsound.PlaySound(sounds[1],winsound.SND_ALIAS)
-        os.system('cls')
+    time.sleep(1)
 
 Fib_Num()
