58132ec3990c424d817f81a503279a75d9ea567b
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-12-27 23:04:37 -0500

    Add files via upload

--- Screen Scheme.py
+++ Screen Scheme.py
@@ -13,7 +13,7 @@ text_colours=(
   '\033[38;2;255;0;255m', # index 0 = red
   '\033[38;2;0;255;0m', # index 1 = green
   '\033[38;2;255;255;0m', # index 2 = yellow
-  '\033[38;2;0;255;255m', # index 3 = cyan    )
+  '\033[38;2;0;255;255m', # index 3 = cyan
+  )
 
 anti_flick = '\033[H'+'\033[?25l'
-
