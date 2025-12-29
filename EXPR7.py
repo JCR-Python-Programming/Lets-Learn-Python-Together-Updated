12f4b6070040dd1fa70114034a84054f072c7420
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2025-12-29 14:23:33 -0500

    Add files via upload

--- EXPR7.py
+++ EXPR7.py
@@ -1,31 +1,30 @@
-
 class Mom:
-    def __init__(self, mom1, mom2, mom3):
+    def __init__(self, mom1,mom2,mom3):
         self.mom1 = mom1
         self.mom2 = mom2
         self.mom3 = mom3
 
 class Dad:
-    def __init__(self, dad1, dad2, dad3):
+    def __init__(self,dad1,dad2,dad3):
         self.dad1 = dad1
         self.dad2 = dad2
         self.dad3 = dad3
 
-class Child(Dad, Mom):
-    def __init__(self, dad1, dad2, dad3, mom1, mom2, mom3):
-        Dad.__init__(self, dad1, dad2, dad3)
-        Mom.__init__(self, mom1, mom2, mom3)
+class Child(Mom):
+    def __init__(self,dad1,dad2,dad3,mom1,mom2,mom3):
+        Mom.__init__(self,mom1,mom2,mom3)
+        Dad.__init__(self,dad1,dad2,dad3)
 
-mom = Mom('Test1', 'Test2', 'Test3')
+mom = Mom('Mom1','Mom2','Mom3')
 
-dad = Dad('Test1', 'Test2', 'Test3')
+dad = Dad('Dad1', 'Dad2','Dad3')
 
-child = Child('Test1', 'Test2', 'Test3','Test4', 'Test5', 'Test6')
+child = Child('Dad1','Dad2','Dad3','Mom1', 'Mom2','Mom3')
 
 print(mom.mom1)
 
-print(dad.dad1)
-
-print(child.dad1)
+print(dad.dad2)
 
 print(child.mom1)
+
+print(child.dad2)
