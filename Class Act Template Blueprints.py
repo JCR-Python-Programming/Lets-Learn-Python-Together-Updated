f77d9acb9496c7fe31e70ff076c244adc54fbc28
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2026-01-19 16:34:43 -0500

    Add files via upload

--- Class Act Template Blueprints.py
+++ Class Act Template Blueprints.py
@@ -670,6 +670,144 @@ print(class_all.sub_two3)
 print(class_all.sub_three3)
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 class Main:
+  def __init__(self,**kwargs):
+
+    self.main1 = 'main1'
+    self.main2 = 'main2'
+    self.main3 = 'main3'
+
+class Sub_one(Main):
+  def __init__(self,**kwargs):
+
+    self.sub_one1 = 'sub_one1'
+    self.sub_one2 = 'sub_one2'
+    self.sub_one3 = 'sub_one3'
+    super().__init__(**kwargs)
+
+class Sub_two(Sub_one):
+  def __init__(self,**kwargs):
+
+    self.sub_two1 = 'sub_two1'
+    self.sub_two2 = 'sub_two2'
+    self.sub_two3 = 'sub_two3'
+    super().__init__(**kwargs)
+
+class Sub_three(Sub_two):
+  def __init__(self,**kwargs):
+
+    self.sub_three1 = 'sub_three1'
+    self.sub_three2 = 'sub_three2'
+    self.sub_three3 = 'sub_three3'
+    super().__init__(**kwargs)
+
+class Class_all(Sub_three,Sub_two,Sub_one,Main):
+  def __init__(
+    self,main1,main2,main3,
+    sub_one1,sub_one2,sub_one3,
+    sub_two1,sub_two2,sub_two3,
+    sub_three1,sub_three2,sub_three3):
+    super().__init__(
+      main1 = main1,main2 = main2,
+      main3 = main3,sub_one1 = sub_one1,
+      sub_one2 = sub_one2,sub_one3 = sub_one3,
+      sub_two1 = sub_two1,sub_two2 = sub_two2,
+      sub_two3 = sub_two3,sub_three1 = sub_three1,
+      sub_three2 = sub_three2,sub_three3 = sub_three3)
+
+main = Main()
+
+sub_one = Sub_one()
+
+sub_two = Sub_two()
+
+sub_three = Sub_three()
+
+class_all = Class_all(
+  'Main value1','Main value2','Main value3',
+  'Sub_one value1','Sub_one value2','Sub_one value3',
+  'Sub_three value1','Sub_three value2','Sub_three value3',
+  'Sub_two value1','Sub_two value2','Sub_two value3')
+
+print(main.main3)
+
+print(sub_one.sub_one3)
+
+print(sub_two.sub_two3)
+
+print(sub_three.sub_three3)
+
+print(class_all.sub_three3)
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+class Main:
+  def __init__(self,**kwargs):
+
+    self.main1 = 'main1'
+    self.main2 = 'main2'
+    self.main3 = 'main3'
+
+class Sub_one:
+  def __init__(self,**kwargs):
+
+    self.sub_one1 = 'sub_one1'
+    self.sub_one2 = 'sub_one2'
+    self.sub_one3 = 'sub_one3'
+    super().__init__(**kwargs)
+
+class Sub_two:
+  def __init__(self,**kwargs):
+
+    self.sub_two1 = 'sub_two1'
+    self.sub_two2 = 'sub_two2'
+    self.sub_two3 = 'sub_two3'
+    super().__init__(**kwargs)
+
+class Sub_three:
+  def __init__(self,**kwargs):
+
+    self.sub_three1 = 'sub_three1'
+    self.sub_three2 = 'sub_three2'
+    self.sub_three3 = 'sub_three3'
+    super().__init__(**kwargs)
+
+class Class_all(Sub_three,Sub_two,Sub_one,Main):
+  def __init__(
+    self,main1,main2,main3,
+    sub_one1,sub_one2,sub_one3,
+    sub_two1,sub_two2,sub_two3,
+    sub_three1,sub_three2,sub_three3):
+    super().__init__(
+      main1 = main1,main2 = main2,
+      main3 = main3,sub_one1 = sub_one1,
+      sub_one2 = sub_one2,sub_one3 = sub_one3,
+      sub_two1 = sub_two1,sub_two2 = sub_two2,
+      sub_two3 = sub_two3,sub_three1 = sub_three1,
+      sub_three2 = sub_three2,sub_three3 = sub_three3)
+
+main = Main()
+
+sub_one = Sub_one()
+
+sub_two = Sub_two()
+
+sub_three = Sub_three()
+
+class_all = Class_all(
+  'Main value1','Main value2','Main value3',
+  'Sub_one value1','Sub_one value2','Sub_one value3',
+  'Sub_three value1','Sub_three value2','Sub_three value3',
+  'Sub_two value1','Sub_two value2','Sub_two value3')
+
+print(main.main3)
+
+print(sub_one.sub_one3)
+
+print(sub_two.sub_two3)
+
+print(sub_three.sub_three3)
+
+print(class_all.main3)
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+class Main:
   def __init__(
     self,main1 = '',main2 = '',main3 = '',**kwargs):
 
@@ -706,6 +844,86 @@ class Sub_three:
     super().__init__(**kwargs)
 
 class Class_all(Main,Sub_one,Sub_two,Sub_three):
+  def __init__(
+    self,main1,main2,main3,
+    sub_one1,sub_one2,sub_one3,
+    sub_two1,sub_two2,sub_two3,
+    sub_three1,sub_three2,sub_three3):
+    super().__init__(
+      main1 = 'main1',main2 = 'main2',
+      main3 = 'main3',sub_one1 = 'sub_one1',
+      sub_one2 = 'sub_one2',sub_one3 = 'sub_one3',
+      sub_two1 = 'sub_two1',sub_two2 = 'sub_two2',
+      sub_two3 = 'sub_two3',sub_three1 = 'sub_three1',
+      sub_three2 = 'sub_three2',sub_three3 = 'sub_three3')
+
+main = Main('Main value1','Main value2','Main value3')
+
+sub_one = Sub_one('Sub_one value1','Sub_one value2','Sub_one value3')
+
+sub_two = Sub_two('Sub_two value1','Sub_two value2','Sub_two value3')
+
+sub_three = Sub_three('Sub_three value1','Sub_three value2','Sub_three value3')
+
+class_all = Class_all(
+  'Main value1','Main value2','Main value3',
+  'Sub_one value1','Sub_one value2','Sub_one value3',
+  'Sub_two value1','Sub_two value2','Sub_two value3',
+  'Sub_three value1','Sub_three value2','Sub_three value3')
+
+print(main.main3)
+
+print(sub_one.sub_one3)
+
+print(sub_two.sub_two3)
+
+print(sub_three.sub_three3)
+
+print(class_all.main3)
+
+print(class_all.sub_one3)
+
+print(class_all.sub_two3)
+
+print(class_all.sub_three3)
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+class Main:
+  def __init__(
+    self,main1 = '',main2 = '',main3 = '',**kwargs):
+
+    self.main1 = main1
+    self.main2 = main2
+    self.main3 = main3
+    super().__init__(**kwargs)
+
+class Sub_one(Main):
+  def __init__(
+    self,sub_one1 = '',sub_one2 = '',sub_one3 = '',**kwargs):
+
+    self.sub_one1 = sub_one1
+    self.sub_one2 = sub_one2
+    self.sub_one3 = sub_one3
+    super().__init__(**kwargs)
+
+class Sub_two(Sub_one):
+  def __init__(
+    self,sub_two1 = '',sub_two2 = '',sub_two3 = '',**kwargs):
+
+    self.sub_two1 = sub_two1
+    self.sub_two2 = sub_two2
+    self.sub_two3 = sub_two3
+    super().__init__(**kwargs)
+
+class Sub_three(Sub_two):
+  def __init__(
+    self,sub_three1 = '',sub_three2 = '',sub_three3 = '',**kwargs):
+
+    self.sub_three1 = sub_three1
+    self.sub_three2 = sub_three2
+    self.sub_three3 = sub_three3
+    super().__init__(**kwargs)
+
+class Class_all(Sub_three,Sub_two,Sub_one,Main):
   def __init__(
     self,main1,main2,main3,
     sub_one1,sub_one2,sub_one3,
