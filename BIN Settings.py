cb92eeb88a40a1db5f336fd82e4a5f0eb81ff610
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2026-03-29 07:08:51 -0400

    Add files via upload

--- BIN Settings.py
+++ BIN Settings.py
@@ -4,31 +4,31 @@ lsb=8_388_607,8_388_608  # least significant bits
 for i in range(msb[0],lsb[0],-1):
   bin=f'{i:b}'
   print('\n',len(f'{msb[0]-i:b}'),
-    f'bits = Bin: {msb[0]-i:024b}\n\n',
-    f'= Hex: {msb[0]-i:X}\n',
-    f'= Oct: {msb[0]-i:o}\n',
+    f'bits = Bin: {msb[0]-i:024b}\n\n'
+    f'= Hex: {msb[0]-i:X}\n'
+    f'= Oct: {msb[0]-i:o}\n'
     f'= Dec: {msb[0]-i:d}')
 
 for i in range(lsb[1],msb[1]):
   bin=f'{i:b}'
   print('\n',len(f'{msb[0]:b}'),
-    f'bits = Bin: {i:024b}\n\n',
-    f'= Hex: {i:X}\n',
-    f'= Oct: {i:o}\n',
+    f'bits = Bin: {i:024b}\n\n'
+    f'= Hex: {i:X}\n'
+    f'= Oct: {i:o}\n'
     f'= Dec: {i:d}')
 
 for i in range(msb[0],lsb[0],-1):
   bin=f'{i:b}'
   print('\n',len(f'{msb[0]-i:b}'),
-    f'bits = Bin: {(i & 0xffffff):024b}\n\n',
-    f'= Hex: -{msb[0]-i:X}\n',
-    f'= Oct: -{msb[0]-i:o}\n',
+    f'bits = Bin: {(i & 0xffffff):024b}\n\n'
+    f'= Hex: -{msb[0]-i:X}\n'
+    f'= Oct: -{msb[0]-i:o}\n'
     f'= Dec: -{msb[0]-i:d}')
 
 for i in range(lsb[1],msb[1]):
   bin=f'{i:b}'
   print('\n',len(f'{msb[0]:b}'),
-    f'bits = Bin: {(i & 0xffffff):024b}\n\n',
-    f'= Hex: -{i:X}\n',
-    f'= Oct: -{i:o}\n',
+    f'bits = Bin: {(i & 0xffffff):024b}\n\n'
+    f'= Hex: -{i:X}\n'
+    f'= Oct: -{i:o}\n'
     f'= Dec: -{i:d}')
